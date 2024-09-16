ae710dd9b1c2468fbcdbe05d573625232e721241
Neeraj Verma <github@smurfpandey.me>
2024-09-16 23:04:32 +0530

    set default value for env vars

--- bills_collector/config.py
+++ bills_collector/config.py
@@ -15,14 +15,14 @@ class Config(object):
     # FLASK_APP = env.str('FLASK_APP')
     ENV = 'development'
     DEBUG = False
-    SECRET_KEY = env.str("SECRET_KEY")
-    APP_BASE_URL = env.str('APP_BASE_URL') # Base URL of the app
+    SECRET_KEY = env.str('SECRET_KEY', '')
+    APP_BASE_URL = env.str('APP_BASE_URL', '') # Base URL of the app
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
     LOG_LEVEL = env.str('LOG_LEVEL', default="INFO")
     PREFERRED_URL_SCHEME = 'https'
 
     # Flask-SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL', '')
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -30,17 +30,17 @@ class Config(object):
     STATIC_FOLDER = '/static/dist'
 
     #Oauth Client Deets
-    GOOGLE_CLIENT_ID = env.str('GOOGLE_CLIENT_ID')
-    GOOGLE_CLIENT_SECRET = env.str('GOOGLE_CLIENT_SECRET')
+    GOOGLE_CLIENT_ID = env.str('GOOGLE_CLIENT_ID', '')
+    GOOGLE_CLIENT_SECRET = env.str('GOOGLE_CLIENT_SECRET', '')
 
     #Oauth Client Deets
-    ZOHO_CLIENT_ID = env.str('ZOHO_CLIENT_ID')
-    ZOHO_CLIENT_SECRET = env.str('ZOHO_CLIENT_SECRET')
+    ZOHO_CLIENT_ID = env.str('ZOHO_CLIENT_ID', '')
+    ZOHO_CLIENT_SECRET = env.str('ZOHO_CLIENT_SECRET', '')
     ZOHO_AUTHORIZE_URL = 'https://accounts.zoho.com/oauth/v2/auth'
     ZOHO_ACCESS_TOKEN_URL = 'https://accounts.zoho.com/oauth/v2/token'
 
     # Celery
-    broker_url = env.str('CELERY_BROKER_URL')
+    broker_url = env.str('CELERY_BROKER_URL', '')
     # result_backend = env.str('CELERY_RESULT_BACKEND')
 
 class ProductionConfig(Config):
